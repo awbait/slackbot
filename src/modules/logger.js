dab3136b19b53d0626803676fe8af03234f5d807
Glitch (awbait-slackbot) <none>
2019-11-26 09:23:15 +0000

    🐠👢 Checkpoint ./src/app.js:8369563/8 ./src/modules/logger.js:8369563/80

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,8 +1,8 @@
 import { configure, getLogger } from 'log4js';
 
 configure({
-  appenders: { console: { type: 'console' }, fileAppender: { type: 'file', filename: '../logs/server.log' } },
-  categories: { default: { appenders: ['console', 'fileAppender'], level: 'trace' } },
+  appenders: { console: { type: 'console' } },
+  categories: { default: { appenders: ['console'], level: 'trace' } },
 });
 
 const logger = getLogger();
